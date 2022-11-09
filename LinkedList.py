cbde86da5ff0ca4e9faa88fcaa03f98ef4448f98
Dennis <lamden@oregonstate.edu>
2022-11-09 14:32:38 -0700

    plain list editing logic

--- LinkedList.py
+++ LinkedList.py
@@ -153,8 +153,8 @@ class LinkedList:
         Recursive to_plain_list helper method.
         """
         plain_list = []
-        if self._head is not None:
-            return self.rec_to_plain_list(self._head, plain_list)
+        if self._head is None:
+            return plain_list
 
         else:
-            return plain_list
+            return self.rec_to_plain_list(self._head, plain_list)
