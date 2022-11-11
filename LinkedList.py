8ba9027e6fb399525ba3e4b6163bce6272b19be0
Dennis <lamden@oregonstate.edu>
2022-11-10 19:09:14 -0700

    changing reverse logic

--- LinkedList.py
+++ LinkedList.py
@@ -129,8 +129,7 @@ class LinkedList:
         Recursive reverse helper method.
         """
         if self._head is None:
-            return
-        self.rec_reverse(self._head)
+            return self.rec_reverse(self._head)
 
     def rec_to_plain_list(self, curr, plain_list):
         """
